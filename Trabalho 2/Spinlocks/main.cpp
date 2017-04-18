7ce1587165f865f5db192673fe344697d74d6457
Renan Basilio <renanbasilio@poli.ufrj.br>
2017-04-18 11:06:28 -0300

    Spinlocks modificado para ser multiplataforma *WIN32 usa thread ao inves de pthread e ctime ao inves de sys/time.h

--- Trabalho 2/Spinlocks/main.cpp
+++ Trabalho 2/Spinlocks/main.cpp
@@ -1,10 +1,17 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <pthread.h> 
-#include <stdlib.h> 
+
+#ifdef __linux__
+#include <pthread.h>
+#include <sys/time.h>
+#elif _WIN32
+#include <thread>
+#include <ctime>
+#endif
+
 #include <vector> 
 #include <iostream>
-#include <sys/time.h>
+
 #include <atomic>
 
 using namespace std; 
@@ -34,8 +41,14 @@ int main(int argc, char *argv[]){
 	}
 	
 	//Medir tempo
+#ifdef __linux__
     struct timeval tval_before, tval_after, tval_result;
     gettimeofday(&tval_before, NULL);
+#elif _WIN32
+	clock_t tval_before, tval_after;
+	double tval_result;
+	tval_before = clock();
+#endif
 
 	//Alocar memoria para a struct
     struct sum_blocking *sb = (struct sum_blocking *) malloc(sizeof(struct sum_blocking *)); ; 
@@ -44,7 +57,12 @@ int main(int argc, char *argv[]){
 	
     int numThreads = atoi(argv[1]); 
     int numNumbers = atoi(argv[2]); 
-    pthread_t parcels[numThreads];   
+
+#ifdef __linux__
+    pthread_t parcels[numThreads]; 
+#elif _WIN32
+	vector<thread> parcels(numThreads);
+#endif
 	
 	int i; 
 
@@ -53,24 +71,38 @@ int main(int argc, char *argv[]){
     args->size = numNumbers/numThreads; 
     
 
-    for (i=0;i<numThreads;i++){ 
-        //cout << "Criei a thread" << endl; 
-        if(pthread_create(&parcels[i],NULL,sumThread,(void *) args)){ 
+	for (i = 0; i < numThreads; i++) {
+		//cout << "Criei a thread" << endl;
+#ifdef __linux__
+		if (pthread_create(&parcels[i], NULL, sumThread, (void *)args)) {
+#elif _WIN32
+		parcels[i] = thread(sumThread, (void *)args);
+		if (!parcels[i].joinable()) {
+#endif
             cout << "Nao foi possivel criar a thread" << endl; 
             return -1; 
         } 
     }   
 	
     for (i=0;i<numThreads;i++){ 
+#ifdef __linux__
         pthread_join(parcels[i], NULL); 
+#elif _WIN32
+		parcels[i].join();
+#endif
     }
 
 	//Obtem o tempo final e armazena a diferenca 
+#ifdef __linux__
 	gettimeofday(&tval_after, NULL);
 	timersub(&tval_after, &tval_before, &tval_result);
-
-	cout <<"Tempo gasto: " << tval_result.tv_sec << "." << tval_result.tv_usec << endl;     
-	cout <<"Soma total: " << sb->sum << endl;; 
+	cout << "Tempo gasto: " << tval_result.tv_sec << "." << tval_result.tv_usec << endl;
+#elif _WIN32
+	tval_after = clock();
+	tval_result = (double)(tval_after - tval_before) / CLOCKS_PER_SEC;
+	cout << "Tempo gasto: " << tval_result << "s." << endl;
+	cout << "Soma total: " << sb->sum << endl;
+#endif
 	
 	//libera a memoria alocada para as struts
 	free(sb);
@@ -112,5 +144,8 @@ void *sumThread(void *args){
     release(threadArgs->sb); 
 	
 	//Termina a chamada da thread
-    pthread_exit(NULL); 
+#ifdef __linux__
+    pthread_exit(NULL);
+#endif
+	return 0;
 }
